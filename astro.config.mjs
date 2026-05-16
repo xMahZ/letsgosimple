bcd8c68483e1b40a2f10f2dcf45262344612d4b9
Eduardo <eduardo.burrows@hotmail.com>
2026-05-16 12:43:03 -0400

    fixastroconfigredirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,15 @@
-// @ts-check 
-import { defineConfig } from 'astro/config'; import tailwindcss from '@tailwindcss/vite'; // https://astro.build/config 
-export default defineConfig({ vite: { plugins: [tailwindcss()] } });
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  redirects: {
+    // Atajos de desarrollo (no canónicos)
+    "/uf-a-clp": "/cl/finanzas/uf-a-clp",
+    "/otra-ruta": "/cl/finanzas/otra-pagina",
+    "/algo-mas": "/cl/otra-seccion/pagina",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
